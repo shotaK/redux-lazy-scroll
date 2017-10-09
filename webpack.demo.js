9d35137a035381c03f441d5ab32dac422a6ce63e
Shota Karkashadze <sh.karkashadze@gmail.com>
2017-10-09 14:47:28 +0400

    Change the build version to production

--- webpack.demo.js
+++ webpack.demo.js
@@ -7,7 +7,7 @@ module.exports = merge(dev, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('demo')
+        'NODE_ENV': JSON.stringify('production')
       }
     })
   ]
