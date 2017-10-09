3a11eaeab06738a5b32d1349afcbe5f672ccd189
Shota Karkashadze <sh.karkashadze@gmail.com>
2017-10-09 14:28:45 +0400

    Remove Mongo db connections

--- dev/server/config.js
+++
@@ -1,6 +0,0 @@
-var configs = {};
-configs.applicationPort = 3000;
-configs.dbName = 'ReduxInfiniteScrollDB';
-configs.dbHost = 'localhost';
-
-module.exports = configs;
--- dev/server/index.js
+++ dev/server/index.js
@@ -1,17 +1,11 @@
 var express = require('express');
-var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var db = mongoose.connection;
 var cors = require('cors');
 var path = require('path');
 
-var configs = require('./config');
 var routes = require('./routes');
-var helperFunctions = require('./helperFunctions');
 
 var app = express();
-db.on('error', console.error);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,14 +15,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://'+configs.dbHost+'/'+configs.dbName);
-helperFunctions.populateDb();
-
 routes(app);
 
 app.use('/posts',express.static('posts'));
 app.use('/',express.static('client'));
 
-app.listen(configs.applicationPort, function () {
-  console.log('App is running on port: '+configs.applicationPort);
-});
+const port = process.env.PORT || 3000;
+app.listen(port);
